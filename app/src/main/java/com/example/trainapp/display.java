14e0e7c0e52c43f3e61a053dfe64b379fab57010
darkone0112 <darkone0112@gmail.com>
2022-10-28 12:38:57 +0200

    portatil

--- app/src/main/java/com/example/trainapp/display.java
+++ app/src/main/java/com/example/trainapp/display.java
@@ -3,31 +3,45 @@ package com.example.trainapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class display extends AppCompatActivity implements View.OnClickListener {
-    TextView displayBox;
-    Button goBackBtn;
+    TextView displayBox,confirmada;
+    Button goBackBtn, confirm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
         displayBox = findViewById(R.id.displayBox);
+        confirmada = findViewById(R.id.confirmada);
         goBackBtn = findViewById(R.id.goBackBtn);
+        confirm = findViewById(R.id.confirm);
         //display viaje = (display) getIntent().getSerializableExtra("laclave");
         //displayBox.setText(viaje.toString());
         Intent i = getIntent();
         displayBox.setText(i.getStringExtra(MainActivity.laclave));
         goBackBtn.setOnClickListener(this);
+        confirm.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        Intent goBack = new Intent(this, MainActivity.class);
-        startActivity(goBack);
+        switch (view.getId()){
+            case R.id.confirm:
+                confirmada.setText("Orden Confirmada!");
+                //confirmada.getBackground().setColorFilter(Color.GREEN, PorterDuff.Mode.SRC_ATOP);
+                confirmada.setTextColor(Color.GREEN);
+                break;
+            case R.id.goBackBtn:
+                Intent goBack = new Intent(this, MainActivity.class);
+                startActivity(goBack);
+                break;
+
+        }
     }
 }
