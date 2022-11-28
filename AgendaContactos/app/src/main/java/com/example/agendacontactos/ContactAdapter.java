42680f7b317a1be90dd388ffaac9332d892faeed
darkone0112 <darkone0112@gmail.com>
2022-11-29 00:55:41 +0100

    portatil examen

--- AgendaContactos/app/src/main/java/com/example/agendacontactos/ContactAdapter.java
+++ AgendaContactos/app/src/main/java/com/example/agendacontactos/ContactAdapter.java
@@ -4,13 +4,17 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.view.menu.MenuView;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
@@ -34,14 +38,14 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.txtName.setText(contact.get(position).getName());
+        holder.txtEmail.setText(contact.get(position).getEmail());
         holder.parent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(context,contact.get(holder.getAdapterPosition()).getName() + " Selected", Toast.LENGTH_SHORT).show();
             }
         });
-
-
+        Glide.with(context).asBitmap().load(contact.get(position).getImageUrl()).into(holder.image);
     }
 
     @Override
@@ -55,11 +59,14 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-        private TextView txtName;
-        private RelativeLayout parent;
+        private TextView txtName,txtEmail;
+        private ImageView image;
+        private CardView parent;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             txtName = itemView.findViewById(R.id.txtName);
+            txtEmail = itemView.findViewById(R.id.txtEmail);
+            image = itemView.findViewById(R.id.image);
             parent = itemView.findViewById(R.id.parent);
         }
     }
--- AgendaContactos/app/src/main/java/com/example/agendacontactos/MainActivity.java
+++ AgendaContactos/app/src/main/java/com/example/agendacontactos/MainActivity.java
@@ -17,13 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MyRecycleView = findViewById(R.id.MyRecycleView);
-
         ArrayList<Contact> contacts = new ArrayList<>();
         contacts.add(new Contact("pepe","pepe@gmail.com","https://static.cnews.fr/sites/default/files/pepe_the_frog_sad.jpg"));
         contacts.add(new Contact("susi","verysusi@gmail.com","https://files.cults3d.com/uploaders/15629801/illustration-file/b585df30-4634-4ed6-9d0c-769cebc23b95/macizo1.png"));
         ContactAdapter adapter = new ContactAdapter(this);
         adapter.setContact(contacts);
         MyRecycleView.setAdapter(adapter);
-        MyRecycleView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        MyRecycleView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
